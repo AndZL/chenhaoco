d539215e341fce067ec9215afc63c4d05a4cdb53
Administrator <18519330848@163.com>
2016-12-14 10:46:03 +0800

    qwe

--- app/src/main/java/com/example/ch/chproject/MainActivity.java
+++ app/src/main/java/com/example/ch/chproject/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("","guo");
+        Log.i("","ZhaoKai");
     }
 }
