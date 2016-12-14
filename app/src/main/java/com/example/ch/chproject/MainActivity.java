ff80ec6709f1f26c0fd262909f08709c933c27e8
StillTmac <13165552818@163.com>
2016-12-14 10:47:19 +0800

    first commit

--- app/src/main/java/com/example/ch/chproject/MainActivity.java
+++ app/src/main/java/com/example/ch/chproject/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this,"Optimus Primal    ",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this,"Optimus Primal",Toast.LENGTH_LONG).show();
 
     }
 }
