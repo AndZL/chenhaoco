a70f25e1c2dd4ab9bcd1b025a13ee8fd53a213bf
ch1991 <changhua0610@163.com>
2016-12-14 11:33:25 +0800

    hh

---
+++ app/src/main/java/com/example/ch/chproject/FiveGroup.java
@@ -0,0 +1,12 @@
+package com.example.ch.chproject;
+
+/**
+ * Created by lenovo on 2016/12/14.
+ */
+
+public class FiveGroup {
+    public void showMembers(){
+        String fengxiang="逢翔";
+       String changhua="常华";
+    }
+}
