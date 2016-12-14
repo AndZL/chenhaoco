c46ae3625b2871b39d1c15e161de7186bc596a10
loveLian <15094068036@163.com>
2016-12-14 11:22:46 +0800

    咚咚

--- app/src/main/java/com/example/ch/chproject/MainActivity.java
+++ app/src/main/java/com/example/ch/chproject/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("","guo");
+        String Nice="hope and dream";
         Log.i("","ZhaoKai");
     }
 }
